73e770f895f2ceca005612e97fc9412cca4fb836
Devohka <110058157+Devohka@users.noreply.github.com>
2024-04-26 23:45:04 +0300

    yes?

--- index.js
+++ index.js
@@ -1,102 +1,74 @@
-
-import { defaultModules, error } from "@pnotify/core";
-import * as PNotifyMobile from "@pnotify/mobile";
-
-defaultModules.set(PNotifyMobile, {});
-
-const countryInputEl = document.querySelector(".country__input");
-const countryTitleEl = document.querySelector(".country__title");
-const countryCapitalEl = document.querySelector(".country-capital");
-const countryPopulationEl = document.querySelector(".country-population");
-const countryLanguagesListEl = document.querySelector(".languages__list");
-const countryImgEl = document.querySelector(".country__img");
-const buttonsListEl = document.querySelector(".buttons__list");
+var $ltMAx$pnotifycore = require("@pnotify/core");
+var $ltMAx$pnotifymobile = require("@pnotify/mobile");
 
 
-const fetchCountries = _.debounce((searchQuery) => {
-  fetch(`https://restcountries.com/v3.1/name/${searchQuery}`)
-    .then((res) => {
-      if (res !== true) {
-        console.log("res !== true");
-      }
-      return res.json();
-    })
-    .then((data) => {
-      const dataLang = data[0].languages;
-      if (data.length > 10) {
-        error({
-          text: "Too many matches found. Please enter a more specific query!",
+
+(0, $ltMAx$pnotifycore.defaultModules).set($ltMAx$pnotifymobile, {});
+const $4fa36e821943b400$var$countryInputEl = document.querySelector(".country__input");
+const $4fa36e821943b400$var$countryTitleEl = document.querySelector(".country__title");
+const $4fa36e821943b400$var$countryCapitalEl = document.querySelector(".country-capital");
+const $4fa36e821943b400$var$countryPopulationEl = document.querySelector(".country-population");
+const $4fa36e821943b400$var$countryLanguagesListEl = document.querySelector(".languages__list");
+const $4fa36e821943b400$var$countryImgEl = document.querySelector(".country__img");
+const $4fa36e821943b400$var$buttonsListEl = document.querySelector(".buttons__list");
+const $4fa36e821943b400$var$fetchCountries = _.debounce((searchQuery)=>{
+    fetch(`https://restcountries.com/v3.1/name/${searchQuery}`).then((res)=>{
+        if (res !== true) console.log("res !== true");
+        return res.json();
+    }).then((data)=>{
+        const dataLang = data[0].languages;
+        if (data.length > 10) (0, $ltMAx$pnotifycore.error)({
+            text: "Too many matches found. Please enter a more specific query!"
         });
-      }
-      buttonsListEl.innerHTML = ``;
-      if (data.length >= 2 && data.length <= 10) {
-        buttonsListEl.style.display = "block";
-        data.forEach((country) => {
-          buttonsListEl.insertAdjacentHTML(
-            "beforeend",
-            `<li><button class=country__button>${country.name.common}</button></li>`
-          );
-        });
-        buttonsListEl.addEventListener("click", (e) => {
-          if (e.target.nodeName === "LI" || e.target.nodeName === "UL") {
-            return;
-          }
-          buttonsListEl.style.display = "none";
-          const index = data.findIndex(
-            (country) => country.name.common === e.target.textContent
-          );
-          countryTitleEl.textContent = data[index].name.common;
-          countryCapitalEl.textContent = data[index].capital;
-          countryPopulationEl.textContent = data[index].population;
-          countryImgEl.innerHTML = `<img src=${data[index].flags.png} alt=${data[index].alt}/>`;
-
-          countryLanguagesListEl.innerHTML = ``;
-
-          if (Object.keys(dataLang).length > 1) {
-            let languagesContent = ``;
-            Object.values(dataLang).forEach((language) => {
-              languagesContent += `<li class=languages__item>${language}</li>`;
+        $4fa36e821943b400$var$buttonsListEl.innerHTML = ``;
+        if (data.length >= 2 && data.length <= 10) {
+            $4fa36e821943b400$var$buttonsListEl.style.display = "block";
+            data.forEach((country)=>{
+                $4fa36e821943b400$var$buttonsListEl.insertAdjacentHTML("beforeend", `<li><button class=country__button>${country.name.common}</button></li>`);
             });
-            countryLanguagesListEl.innerHTML = ``;
-            return (countryLanguagesListEl.innerHTML = languagesContent);
-          }
-          countryLanguagesListEl.insertAdjacentHTML(
-            "beforeend",
-            `<li class=languages__item>${Object.values(dataLang)}</li>`
-          );
-        });
-      }
-
-      if (data.length === 1) {
-        countryTitleEl.textContent = data[0].name.common;
-        countryCapitalEl.textContent = data[0].capital;
-        countryPopulationEl.textContent = data[0].population;
-        countryImgEl.innerHTML = `<img src=${data[0].flags.png} alt=${data[0].alt}/>`;
-
-        countryLanguagesListEl.innerHTML = ``;
-
-        if (Object.keys(data[0].languages).length > 1) {
-          let languagesContent = ``;
-          Object.values(data[0].languages).forEach((language) => {
-            languagesContent += `<li class=languages__item>${language}</li>`;
-          });
-          countryLanguagesListEl.innerHTML = ``;
-          return (countryLanguagesListEl.innerHTML = languagesContent);
+            $4fa36e821943b400$var$buttonsListEl.addEventListener("click", (e)=>{
+                if (e.target.nodeName === "LI" || e.target.nodeName === "UL") return;
+                $4fa36e821943b400$var$buttonsListEl.style.display = "none";
+                const index = data.findIndex((country)=>country.name.common === e.target.textContent);
+                $4fa36e821943b400$var$countryTitleEl.textContent = data[index].name.common;
+                $4fa36e821943b400$var$countryCapitalEl.textContent = data[index].capital;
+                $4fa36e821943b400$var$countryPopulationEl.textContent = data[index].population;
+                $4fa36e821943b400$var$countryImgEl.innerHTML = `<img src=${data[index].flags.png} alt=${data[index].alt}/>`;
+                $4fa36e821943b400$var$countryLanguagesListEl.innerHTML = ``;
+                if (Object.keys(dataLang).length > 1) {
+                    let languagesContent = ``;
+                    Object.values(dataLang).forEach((language)=>{
+                        languagesContent += `<li class=languages__item>${language}</li>`;
+                    });
+                    $4fa36e821943b400$var$countryLanguagesListEl.innerHTML = ``;
+                    return $4fa36e821943b400$var$countryLanguagesListEl.innerHTML = languagesContent;
+                }
+                $4fa36e821943b400$var$countryLanguagesListEl.insertAdjacentHTML("beforeend", `<li class=languages__item>${Object.values(dataLang)}</li>`);
+            });
         }
-        countryLanguagesListEl.insertAdjacentHTML(
-          "beforeend",
-          `<li class=languages__item>${Object.values(data[0].languages)}</li>`
-        );
-      }
-
-    })
-    .catch((err) => {
-      console.log(err);
+        if (data.length === 1) {
+            $4fa36e821943b400$var$countryTitleEl.textContent = data[0].name.common;
+            $4fa36e821943b400$var$countryCapitalEl.textContent = data[0].capital;
+            $4fa36e821943b400$var$countryPopulationEl.textContent = data[0].population;
+            $4fa36e821943b400$var$countryImgEl.innerHTML = `<img src=${data[0].flags.png} alt=${data[0].alt}/>`;
+            $4fa36e821943b400$var$countryLanguagesListEl.innerHTML = ``;
+            if (Object.keys(data[0].languages).length > 1) {
+                let languagesContent = ``;
+                Object.values(data[0].languages).forEach((language)=>{
+                    languagesContent += `<li class=languages__item>${language}</li>`;
+                });
+                $4fa36e821943b400$var$countryLanguagesListEl.innerHTML = ``;
+                return $4fa36e821943b400$var$countryLanguagesListEl.innerHTML = languagesContent;
+            }
+            $4fa36e821943b400$var$countryLanguagesListEl.insertAdjacentHTML("beforeend", `<li class=languages__item>${Object.values(data[0].languages)}</li>`);
+        }
+    }).catch((err)=>{
+        console.log(err);
     });
 }, 1000);
-
-
-
-countryInputEl.addEventListener("input", (element) => {
-  fetchCountries(element.target.value);
+$4fa36e821943b400$var$countryInputEl.addEventListener("input", (element)=>{
+    $4fa36e821943b400$var$fetchCountries(element.target.value);
 });
+
+
+//# sourceMappingURL=index.js.map
